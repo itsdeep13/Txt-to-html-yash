973996f3712725517fad8f1be8db8be9812ef678
itsdeep13 <80030947+itsdeep13@users.noreply.github.com>
2025-10-08 22:20:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def hello_world():
               />▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀<br 
 	      /><br
               /><br>
-                <b>Powered By SAINI BOTS</b>
+                <b>Powered By Mighty Atom</b>
 		</center>
             </p>
         </a>
